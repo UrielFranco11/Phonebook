550769bd0f28e60623aac4586192421ed83a5fe5
Diana Vergara Gómez <divego12@gmail.com>
2026-09-07 16:02:24 -0600

    Se agregó las historias 5 y 6

--- Phonebook/src/test/java/com/junkyard/test/PhonebookTest.java
+++ Phonebook/src/test/java/com/junkyard/test/PhonebookTest.java
@@ -31,4 +31,33 @@ public class PhonebookTest {
         assertTrue(phonebook.addContact(contact));
         assertEquals(1, phonebook.size());
     }
+
+    // HU-05: contactExists(String name)
+    @Test
+    void contactExists_shouldReturnTrue_whenContactExists() {
+        Phonebook phonebook = new Phonebook();
+        phonebook.addContact(new Contact("Uri", "5545455689"));
+        assertTrue(phonebook.contactExists("Uri"));
+    }
+
+    @Test
+    void contactExists_shouldReturnFalse_whenContactDoesNotExist() {
+        Phonebook phonebook = new Phonebook();
+        assertFalse(phonebook.contactExists("Luis"));
+    }
+
+    // HU-06: listContacts() -- imprime por consola, no retorna nada
+    @Test
+    void listContacts_shouldNotThrow_whenPhonebookIsEmpty() {
+        Phonebook phonebook = new Phonebook();
+        assertDoesNotThrow(phonebook::listContacts);
+    }
+
+    @Test
+    void listContacts_shouldNotThrow_whenPhonebookHasContacts() {
+        Phonebook phonebook = new Phonebook();
+        phonebook.addContact(new Contact("Uri", "5545455689"));
+        assertDoesNotThrow(phonebook::listContacts);
+    }
+
 }
