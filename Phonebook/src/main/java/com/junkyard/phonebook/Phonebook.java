f1958be811e425af8226a7891852f2453e66142a
zxfay <aldobriones25@gmail.com>
2026-09-07 13:20:59 -0600

    added  GetContact and DeleteContact

--- Phonebook/src/main/java/com/junkyard/phonebook/Phonebook.java
+++ Phonebook/src/main/java/com/junkyard/phonebook/Phonebook.java
@@ -1,4 +1,23 @@
 package com.junkyard.phonebook;
 
 public class Phonebook {
+    public String GetContact(String contact){
+
+
+        return "buscando contacto de" + contact;
+
+    }
+
+    public String DeleteContact(String contact){
+
+
+        return  "se elimino el contacto de " + contact;
+    }
+
+    public static void main {
+        Phonebook phonebook = New.Phonebook();
+        String resultado = phonebook.GetContact("Aldo Briones");
+        System.out.println(resultado);
+    }
+
 }
