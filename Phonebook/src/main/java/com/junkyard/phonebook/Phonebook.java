c63f39dbee21526d27b73a75159d81d9b087299f
UrielFranco <urielfranco11@outlook.com>
2026-09-07 16:49:59 -0600

    Menu Ultimate

--- Phonebook/src/main/java/com/junkyard/phonebook/Phonebook.java
+++ Phonebook/src/main/java/com/junkyard/phonebook/Phonebook.java
@@ -58,23 +58,23 @@ public class Phonebook {
                 return c;
             }
         }
-            return null;
+        return null;
     }
 
     public boolean deleteContact(String name){
-contact toDelete = searchContact(name);
-if(toDelete != null){
-    return contacts.remove(toDelete);
-}
+        Contact toDelete = searchContact(name);
+        if (toDelete != null) {
+            return contacts.remove(toDelete);
+        }
         return false;
     }
 
     public boolean phonebookIsFull(){
-        return false;
+        return contacts.size() >= maxSize;
     }
 
     public int spaceAvailable(){
-        return 0;
+        return maxSize - contacts.size();
     }
 
     public int size() {
