d369d101c508b28e82d282140e6e13849c9f9b3b
UrielFranco <urielfranco11@outlook.com>
2026-09-07 13:53:35 -0600

    Implement agenda creation and contact addition

--- Phonebook/src/test/java/com/junkyard/test/ContactTest.java
+++ Phonebook/src/test/java/com/junkyard/test/ContactTest.java
@@ -1,4 +1,22 @@
 package com.junkyard.test;
 
+import com.junkyard.contact.Contact;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ContactTest {
+
+    @Test
+    void shouldCreateContactWithNameAndPhone() {
+        Contact contact = new Contact("Uri", "5545455689");
+        assertEquals("Uri", contact.getName());
+        assertEquals("5545455689", contact.getPhone());
+    }
+
+    @Test
+    void twoContactsWithTheSameNameShouldBeEqual() {
+        Contact c1 = new Contact("Uri", "5545455689");
+        Contact c2 = new Contact("Uri", "5545455679");
+        assertEquals(c1, c2); // required by "a contact equals another when names match"
+    }
 }
