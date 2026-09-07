1d514bfec2767ff36a1379dea78ff81117687925
zxfay <aldobriones25@gmail.com>
2026-09-07 13:28:28 -0600

    subo mi rama

--- Phonebook/src/main/java/com/junkyard/phonebook/Phonebook.java
+++ Phonebook/src/main/java/com/junkyard/phonebook/Phonebook.java
@@ -1,22 +1,22 @@
 package com.junkyard.phonebook;
 
 public class Phonebook {
-    public String GetContact(String contact){
+    public String getContact(String contact){
 
 
-        return "buscando contacto de" + contact;
+        return "buscando contacto de " + contact;
 
     }
 
-    public String DeleteContact(String contact){
+    public String deleteContact(String contact){
 
 
         return  "se elimino el contacto de " + contact;
     }
 
-    public static void main {
-        Phonebook phonebook = New.Phonebook();
-        String resultado = phonebook.GetContact("Aldo Briones");
+    public static void main(String[] args) {
+        Phonebook phonebook = new Phonebook();
+        String resultado = phonebook.getContact(" Aldo Briones");
         System.out.println(resultado);
     }
 
